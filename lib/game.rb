5b96366118b1fed5e96ea7b4712bf48d30401ecd
Greg Stimson <stimsong@yahoo.com>
2018-04-24 18:06:02 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -21,7 +21,7 @@ class Game
    end
   # def initialize(player_1 = Players::Human.new("X"),
   #   player_2 = Players::Human.new("O"), board = Board.new)
-  # 
+  #
   #   @player_1 = player_1
   #   @player_2 = player_2
   #   @board = board
